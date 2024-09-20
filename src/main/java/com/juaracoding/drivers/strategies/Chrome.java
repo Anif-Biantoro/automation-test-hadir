c1d9fd524058d7718ae9804f6631b3fc813077a8
Anif Biantoro <anifbiantoro99@gmail.com>
2024-09-20 19:36:31 +0700

    change path chromedriver

--- src/main/java/com/juaracoding/drivers/strategies/Chrome.java
+++ src/main/java/com/juaracoding/drivers/strategies/Chrome.java
@@ -8,7 +8,7 @@ public class Chrome implements DriverStrategy{
 
     @Override
     public WebDriver setStrategy() {
-        System.setProperty("webdriver.chrome.driver","C://chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\MyTools\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
 //        options.setExperimentalOption("useAutomationExtension", false);
 //        options.addArguments("--no-sandbox");
