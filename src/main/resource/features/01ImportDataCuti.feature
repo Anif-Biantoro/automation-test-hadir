e95f21ed2ea379f1be8d67dfd2b503e09ebc8212
irfan <irfannur328@gmail.com>
2024-09-23 23:21:49 +0700

    ImportTest

--- src/main/resource/features/01ImportDataCuti.feature
+++ src/main/resource/features/01ImportDataCuti.feature
@@ -1,6 +1,11 @@
 Feature: ImportDataCuti functionality
-  Scenario: Import Download Template Cuti
+  Scenario: Import and Download Template Cuti
     Given User Logged in to the Hadir Page
-    When User Logged in to the Hadir Page
+    When Click Import List and Click Import Cuti
     Then User Should Move To Import Cuti Page
     And User Download Template Cuti
+    And User Upload Flie Cuti
+    And User Upload File Cuti With Invalid File
+    Then User Should See Error Message
+    And User Upload File Cuti With Empty File
+    Then User should see error message
--- src/test/java/com/juaracoding/ImportTest.java
+++ src/test/java/com/juaracoding/ImportTest.java
@@ -20,51 +20,70 @@ public class ImportTest {
     private TombolPage tombolPage = new TombolPage();
     private ImportCuti importCuti = new ImportCuti();
 
-    public ImportTest(){
+    public ImportTest() {
         driver = Hooks.driver;
         extentTest = Hooks.extentTest;
         driver.get(Constant.URL);
     }
+
     @Given("User Logged in to the Hadir Page")
-    public void User_Logged_in_to_the_Hadri_Page(){
+    public void User_Logged_in_to_the_Hadri_Page() {
         driver.get("https://magang.dikahadir.com/authentication/login");
-        loginPage.login("admin@hadir.com","admin@hadir");
+        loginPage.login("admin@hadir.com", "admin@hadir");
         loginPage.clickLoginBtn();
-        extentTest.log(LogStatus.PASS,"User Logged in to the Hadir Page");
+        extentTest.log(LogStatus.PASS, "User Logged in to the Hadir Page");
     }
 
     @When("Click Import List and Click Import Cuti")
-    public void Click_Import_List_and_Click_Import_Cuti(){
+    public void Click_Import_List_and_Click_Import_Cuti() {
         importCuti.setMuiImport();
         importCuti.setMuiImportCuti();
-        extentTest.log(LogStatus.PASS,"Click Import List and Click Import Cuti");
-        }
+        extentTest.log(LogStatus.PASS, "Click Import List and Click Import Cuti");
+    }
+
     @Then("User Should Move To Import Cuti Page")
-    public void User_Should_Move_To_Import_Cuti_Page(){
-        Assert.assertEquals(importCuti.getTxtDashboard(),"Import Cuti");
-        extentTest.log(LogStatus.PASS,"User Should Move To Import Cuti Page");
+    public void User_Should_Move_To_Import_Cuti_Page() {
+        Assert.assertEquals(importCuti.getTxtDashboard(), "Import Cuti");
+        extentTest.log(LogStatus.PASS, "User Should Move To Import Cuti Page");
     }
+
     @And("User Download Template Cuti")
-    public void User_Download_Template_Cuti(){
+    public void User_Download_Template_Cuti() {
         importCuti.setDownloadBtn();
-        extentTest.log(LogStatus.PASS,"User Download Template Cuti");
+        extentTest.log(LogStatus.PASS, "User Download Template Cuti");
     }
+
     @And("User Upload Flie Cuti")
-    public void User_Upload_Flie_Cuti(){
+    public void User_Upload_Flie_Cuti() {
         importCuti.setChooseFileValid();
         importCuti.setImportBtn();
-        extentTest.log(LogStatus.PASS,"User Upload Flie Cuti");
+        extentTest.log(LogStatus.PASS, "User Upload Flie Cuti");
     }
+
     @And("User Upload File Cuti With Invalid File")
-    public void User_Upload_File_Cuti_With_Invalid_File(){
+    public void User_Upload_File_Cuti_With_Invalid_File() {
         importCuti.setChooseFileInvalid();
         importCuti.setImportBtn();
-        extentTest.log(LogStatus.PASS,"User Upload File Cuti With Invalid File");
+        extentTest.log(LogStatus.PASS, "User Upload File Cuti With Invalid File");
     }
+
     @Then("User Should See Error Message")
-    public void User_Should_See_Error_Message(){
-        Assert.assertEquals(importCuti.getTxtFailedUpload(),"Gagal import excel");
-        extentTest.log(LogStatus.PASS,"User Should See Error Message");
+    public void User_Should_See_Error_Message() {
+        Assert.assertEquals(importCuti.getTxtFailedUpload(), "Gagal import excel");
+        extentTest.log(LogStatus.PASS, "User Should See Error Message");
 
     }
+
+    @And("User Upload File Cuti With Empty File")
+    public void User_Upload_File_Cuti_With_Empty_File() {
+        importCuti.setImportBtn();
+        extentTest.log(LogStatus.PASS, "User Upload File Cuti With Empty File");
+
     }
+    @Then("User should see error message")
+    public void User_should_see_error_mesage(){
+        importCuti.setBlankFile();
+        Assert.assertEquals(importCuti.setBlankFile(),"false");
+        extentTest.log(LogStatus.PASS, "User should see error message");
+    }
+}
